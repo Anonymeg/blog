4cbdc09dc533a9333544fb0f240ede0205e8db0c
顾鑫 <534819838@qq.com>
2019-11-23 11:01:46 +0800

    第一次提交”交

---
+++ src/components/NavBar.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="nav-bar">
+    <el-row>
+      <el-col :span="12"><img src="../assets/images/logo.png" alt=""></el-col>
+      <el-col :span="12"><div class="grid-content bg-purple-light"></div></el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NavBar'
+}
+</script>
+
+<style scoped>
+.nav-bar {
+  width: 100%;
+  height: 49px;
+  background-color: lightskyblue;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
+// 引入自己的样式
+import './assets/css/index.css'
 
 Vue.config.productionTip = false
 
